8458148f990cb486c88999f7714c7f1e745cc338
Danny <barakadan421@gmail.com>
2023-05-12 02:24:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
 const PORT= 3000
 const routes = require("./routes/index")
+const connectDB = require('./database/db')
 
 app.set('view engine', 'pug')
 
+app.use(bodyParser.json())
+
 app.use(routes)
 
-app.listen(PORT, () => {
-    console.log(`express running`)
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`express running`)
+    })
 })
+
