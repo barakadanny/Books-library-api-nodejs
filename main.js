80e324572c77adfce3d312ebe2bd9de1a519442f
Danny <barakadan421@gmail.com>
2023-05-11 23:37:18 +0300

    Listen to port

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const PORT= 3000
+const routes = require("./routes/index")
+
+app.set('view engine', 'pug')
+
+app.use(routes)
+
+app.listen(PORT, () => {
+    console.log(`express running`)
+})
